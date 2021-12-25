3ad44216b94e3bc5ca04c8ad838e4cc0aa6e5658
Asa <asa0abbad@gmail.com>
2021-12-25 20:02:18 +0200

    version dist folder

--- version.js
+++ version.js
@@ -2,7 +2,10 @@ var Version = require("node-version-assets");
 var versionInstance = new Version({
     assets: [
         'dist/main.css',
-        'dist/fontawesome.css'
+        'dist/fontawesome.css',
+        'dist/main.min.js',
+        'dist/termynal.min.css',
+        'dist/termynal.min.js',
     ],
     grepFiles: [
         './index.html'
